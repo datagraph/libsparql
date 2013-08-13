285ceb5b206cf176fe9be04c6e3742d4d061a10a
Arto Bendiken <arto@bendiken.net>
2013-08-14 01:56:40 +0200

    Implemented the text/boolean writer.

--- src/sparql++/writer/bool.cc
+++ src/sparql++/writer/bool.cc
@@ -13,20 +13,22 @@ namespace {
   public:
     implementation(FILE* stream);
     virtual ~implementation() noexcept override;
-    virtual void begin() override;
-    virtual void finish() override;
-    virtual void begin_head() override;
-    virtual void finish_head() override;
-    virtual void begin_variables() override;
-    virtual void finish_variables() override;
-    virtual void write_variable(const char* name) override;
+    virtual void begin() override {}
+    virtual void finish() override {}
+    virtual void begin_head() override {}
+    virtual void finish_head() override {}
+    virtual void begin_variables() override {}
+    virtual void finish_variables() override {}
+    virtual void write_variable(const char* name) override {}
     virtual void write_boolean(bool value) override;
-    virtual void begin_results() override;
-    virtual void finish_results() override;
-    virtual void begin_result() override;
-    virtual void finish_result() override;
-    virtual void write_binding(const char* name, ...) override; // TODO
+    virtual void begin_results() override {}
+    virtual void finish_results() override {}
+    virtual void begin_result() override {}
+    virtual void finish_result() override {}
+    virtual void write_binding(const char* name, ...) override {} // TODO
     virtual void flush() override;
+  private:
+    FILE* _stream = nullptr;
   };
 }
 
@@ -35,84 +37,22 @@ sparql_writer_for_bool(FILE* const stream,
                        const char* const content_type,
                        const char* const charset) {
   (void)content_type, (void)charset;
-  return new implementation(stream); // TODO
+  return new implementation(stream);
 }
 
-implementation::implementation(FILE* const stream) {
+implementation::implementation(FILE* const stream) 
+  : _stream(stream) {
   assert(stream != nullptr);
-  (void)stream; // TODO
 }
 
-implementation::~implementation() noexcept {
-  // TODO
-}
-
-void
-implementation::begin() {
-  // TODO
-}
-
-void
-implementation::finish() {
-  // TODO
-}
-
-void
-implementation::begin_head() {
-  // TODO
-}
-
-void
-implementation::finish_head() {
-  // TODO
-}
-
-void
-implementation::begin_variables() {
-  // TODO
-}
-
-void
-implementation::finish_variables() {
-  // TODO
-}
-
-void
-implementation::write_variable(const char* const name) {
-  (void)name; // TODO
-}
+implementation::~implementation() noexcept {}
 
 void
 implementation::write_boolean(const bool value) {
-  (void)value; // TODO
-}
-
-void
-implementation::begin_results() {
-  // TODO
-}
-
-void
-implementation::finish_results() {
-  // TODO
-}
-
-void
-implementation::begin_result() {
-  // TODO
-}
-
-void
-implementation::finish_result() {
-  // TODO
-}
-
-void
-implementation::write_binding(const char* const name, ...) {
-  (void)name; // TODO
+  fwrite(value ? "true\n" : "false\n", 1, value ? 5 : 6, _stream);
 }
 
 void
 implementation::flush() {
-  // TODO
+  fflush(_stream);
 }
