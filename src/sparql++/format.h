b67a7f717fa384a21e2c838c0656695ccdb89f01
Arto Bendiken <arto@bendiken.net>
2013-08-23 01:08:36 +0200

    Defined the sparql::format structure type.

--- src/sparql++/format.h
+++ src/sparql++/format.h
@@ -4,6 +4,12 @@
 #define SPARQLXX_FORMAT_H
 
 namespace sparql {
+  struct format {
+    const char* const content_type;
+    const char* const charset;
+    const char* const file_extension;
+    const char* const module_name;
+  };
 }
 
 #endif /* SPARQLXX_FORMAT_H */
